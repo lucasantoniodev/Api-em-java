1bf64abf5a8fa6d9e214c16cafef802ff10e7da8
lucasantoniodev <lucasantoniodev@gmail.com>
2023-01-21 12:50:27 -0300

    API em Java

--- src/main/java/com/algaworks/algalogapi/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalogapi/api/controller/ClienteController.java
@@ -5,9 +5,11 @@ import com.algaworks.algalogapi.domain.repository.ClienteRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,26 +20,49 @@ import java.util.List;
  * Example: Resposta como um .xml ou .json
  * Biblioteca para suporte a ambos: com.fasterxml.jackson.dataformat
  * */
-@RestController
 @AllArgsConstructor // Gera construtor para todas as propriedades
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizarDados(@PathVariable Long id, @RequestBody Cliente cliente) {
+        if (!clienteRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        cliente.setId(id);
+
+        final Cliente clienteUpdated = clienteRepository.save(cliente);
+
+        return ResponseEntity.ok(clienteUpdated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCliente(@PathVariable Long id) {
+        if (!clienteRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        clienteRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> listarClientePorID(@PathVariable long id) {
+        return clienteRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("")
     public List<Cliente> listar() {
         return clienteRepository.findAll();
-    }
-
-    @GetMapping("/cliente")
-    public Cliente listarCliente() {
-        return clienteRepository.findByNome("teco");
-    }
-
-    @GetMapping("/clientesaleatorios")
-    public List<Cliente> listarClientesAleatorios() {
-        return clienteRepository.findByNomeContaining("o");
     }
 }
 
